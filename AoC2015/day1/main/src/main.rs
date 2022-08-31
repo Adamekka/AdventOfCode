17beb3125a2e0df90fbae3a162934ce21f87200a
Adam Cvikl <adam.wasd4@gmail.com>
2022-08-31 18:42:02 +0200

    AoC2015/day1/part2

--- AoC2015/day1/main/src/main.rs
+++ AoC2015/day1/main/src/main.rs
@@ -3,17 +3,23 @@ use std::io::prelude::*;
 
 fn main() {
     let mut floor: i32 = 0;
+    let mut floor2 = 0;
     let mut file = File::open("input.txt").expect("Can't open file!");
     let mut input = String::new();
     file.read_to_string(&mut input)
         .expect("Can't read the file!");
     
-    for i in input.chars() {
+    for (index, i) in input.chars().enumerate() {
         if i == '(' {
             floor += 1;
         } else if i == ')' {
             floor -= 1;
         }
+        if floor == -1 {
+            floor2 = index + 1;
+            break; //to get the 1st answer, remove this line
+        }
     }
     println!("{floor}");
+    println!("{floor2}");
 }
