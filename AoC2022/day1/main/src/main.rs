0c6c077bfa0542366294ea07ccd8d741ab7bfd8b
Adamekka <adam.wasd4@gmail.com>
2023-01-19 13:27:35 +0100

    AoC2022/day1 start trying to make it work

--- AoC2022/day1/main/src/main.rs
+++ AoC2022/day1/main/src/main.rs
@@ -1,3 +1,21 @@
+use std::{fs, str::FromStr};
+
 fn main() {
-    println!("Hello, world!");
+    let text = fs::read_to_string("./src/input.txt").expect("no input.txt");
+    let mut super_vec: Vec<Vec<i32>> = Vec::new();
+
+    for line in text.lines() {
+        let mut line_contents: Vec<i32> = Vec::new();
+
+        if line.is_empty() {
+            super_vec.push(line_contents);
+            // break;
+            // line_contents.push(0);
+        } else {
+            let line: i32 = FromStr::from_str(line).expect("bruh");
+            line_contents.push(line);
+        }
+    }
+
+    println!("{:?}", super_vec);
 }
